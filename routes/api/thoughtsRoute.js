7ebb64c11bd94c6bd67e95e2f782ffb8d60ab022
unknown <aguilera.k02@gmail.com>
2023-02-27 16:06:04 -0500

    added basic routes for thoughts

--- routes/api/thoughtsRoute.js
+++ routes/api/thoughtsRoute.js
@@ -1,7 +1,25 @@
 const router = require("express").Router();
 
-router.use("/", (req, res) => {
-    res.send("Thoughts Home Slash Works!")
+// /api/thoughts
+// GET all thoughts
+// POST a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+router.get("/", (req, res) => {
+    res.send("Get All Thoughts")
+})
+
+// GET a thought by its _id
+// PUT to update a thought by its _id
+// DELETE to remove a thought by its _id
+router.get("/:thoughtId", (req, res) => {
+    res.send(`Get a specific thought with id ${req.params.thoughtId}`)
+})
+
+// /api/thoughts/:thoughtId/reactions
+// POST to create a reaction stored in a single thought's reactions array field
+// DELETE to pull and remove a reaction by the reaction's reactionId value
+
+router.get("/:thoughtId/reactions", (req, res) => {
+    res.send(`You'll create a new reaction with thought Id of ${req.params.thoughtId}`)
 })
 
 module.exports = router;
